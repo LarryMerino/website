0851e21398780580e91f702f4462c383d842eb5a
Larry Merino <merino17@icloud.com>
2025-09-01 15:06:08 +0200

    refactor(app): move layout and page to (home) group route

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar04Page from "@/components/navbar-04/navbar-04";
 
